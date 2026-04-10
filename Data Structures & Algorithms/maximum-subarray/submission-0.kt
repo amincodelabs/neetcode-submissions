6b9002a3e4fb80ddce2424d18b80b512ba581d1a
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-04-10 18:22:15 +0400

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun maxSubArray(nums: IntArray): Int {
+        var maxSum = nums[0]
+        
+        for(i in nums.indices) {
+            var currentSum = 0
+            for(j in i until nums.size) {
+                currentSum += nums[j]
+                maxSum = maxOf(currentSum, maxSum)
+            }
+        }
+
+        return maxSum
+    }
+}
