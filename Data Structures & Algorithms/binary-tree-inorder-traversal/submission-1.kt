61e583785200756527d6f871447fcaf9cfbd41cd
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-04-14 16:56:31 +0400

    Add: binary-tree-inorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-1.kt
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+    val result = mutableListOf<Int>()
+    
+    fun dfs(node: TreeNode?) {
+        if (node == null) return
+        dfs(node.left)
+        result.add(node.`val`)
+        dfs(node.right)
+    }
+    
+    dfs(root)
+    return result
+}
+}
