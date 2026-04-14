38ff7c9ee480cf4fd919be87d9122103ff0c8ba6
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-04-14 12:15:17 +0400

    Add: merge-sorted-array - submission-1

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-1.kt
@@ -0,0 +1,30 @@
+class Solution {
+    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int) {
+        var write = m + n - 1
+        var a = m - 1
+        var b = n - 1
+
+        while(a >= 0 && b >= 0) {
+            if(nums1[a] > nums2[b]) {
+                nums1[write] = nums1[a]
+                a--
+            } else {
+                nums1[write] = nums2[b]
+                b--
+            }
+            write--
+        }
+
+        while(a >= 0) {
+            nums1[write] = nums1[a]
+            a--
+            write--
+        }
+
+        while(b >= 0) {
+            nums1[write] = nums2[b]
+            b--
+            write--
+        }
+    }
+}
