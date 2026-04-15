023cf6b73af0831f6c55932d4eaa53442b2e7007
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-04-15 10:17:07 +0400

    Add: binary-tree-preorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-1.kt
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun preorderTraversal(root: TreeNode?): List<Int> {
+        if (root == null) return emptyList()
+
+        val result = mutableListOf<Int>()
+        val stack = ArrayDeque<TreeNode>()
+
+        stack.addLast(root)
+
+        while(stack.isNotEmpty()) {
+            val node = stack.removeLast()
+            result.add(node.`val`)
+
+            // Push right first so left is processed first
+            node.right?.let { stack.addLast(it) }
+            node.left?.let { stack.addLast(it) }
+        }
+
+        return result
+    }
+}
