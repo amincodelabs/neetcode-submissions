e11a2cde1cd1c1f33bf2fb260626683df91cc2e7
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-04-20 11:45:11 +0400

    Add: evaluate-reverse-polish-notation - submission-0

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.kt
@@ -0,0 +1,68 @@
+class Solution {
+    fun evalRPN(tokens: Array<String>): Int {
+        val stack = Stack()
+
+        fun operands(): Pair<Int, Int> {
+            val b = stack.pop().toInt()
+            val a = stack.pop().toInt()
+            return Pair(a, b)
+        }
+
+        for(token in tokens) {
+            when(token) {
+                "+" -> {
+                    val (a, b) = operands()
+                    stack.push(a + b)
+                }
+                "-" -> {
+                    val (a, b) = operands()
+                    stack.push(a - b)
+                }
+                "*" -> {
+                    val (a, b) = operands()
+                    stack.push(a * b)
+                }
+                "/" -> {
+                    val (a, b) = operands()
+                    stack.push(a / b)
+                }
+                else -> stack.push(token.toInt())
+            }
+        }
+
+        return stack.pop()
+    }
+
+    private fun sum(a: Int, b: Int) = a + b
+}
+
+class Stack(private val initialCapacity: Int = 10) {
+
+    private var elements = IntArray(initialCapacity)
+
+    var size: Int =0
+        private set
+
+    fun push(element: Int) {
+        if(size == elements.size) {
+            growSize()
+        }
+
+        elements[size++] = element 
+    }
+
+    fun pop(): Int {
+        if(size == 0) throw NoSuchElementException("Stack is empty")
+        return elements[--size]
+    }
+
+    fun peek(): Int {
+        if (size == 0) throw NoSuchElementException("Stack is empty")
+        return elements[size - 1]
+    }
+
+    private fun growSize() {
+        val newCapacity = if(elements.size == 0) 1 else elements.size * 2
+        elements = elements.copyOf(newCapacity)
+    }
+}
