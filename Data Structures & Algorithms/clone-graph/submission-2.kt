2d4d2e398d8791c98b9295c3ea3e6cc602176132
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-04-26 10:26:17 +0400

    Add: clone-graph - submission-2

---
+++ Data Structures & Algorithms/clone-graph/submission-2.kt
@@ -0,0 +1,34 @@
+/**
+ * Definition for a Node.
+ * class Node(var `val`: Int) {
+ *     var neighbors: ArrayList<Node?> = ArrayList<Node?>()
+ * }
+ */
+
+// BFS
+class Solution {
+    fun cloneGraph(node: Node?): Node? {
+        if (node == null) return null
+
+        val oldToNew = HashMap<Node, Node>()
+        oldToNew[node] = Node(node.`val`)
+        val queue = ArrayDeque<Node>()
+        queue.add(node)
+
+        while (queue.isNotEmpty()) {
+            val cur = queue.removeFirst()
+
+            for (nei in cur.neighbors) {
+                nei?.let { neighbor ->
+                    if (neighbor !in oldToNew) {
+                        oldToNew[neighbor] = Node(neighbor.`val`)
+                        queue.add(neighbor)
+                    }
+                    oldToNew[cur]?.neighbors?.add(oldToNew[neighbor])
+                }
+            }
+        }
+
+        return oldToNew[node]
+    }
+}
