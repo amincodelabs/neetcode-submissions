4d540655aad2e3418121f30a4613907c419cb44d
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-04-26 09:45:24 +0400

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.kt
@@ -0,0 +1,32 @@
+/*
+Definition for a Node.
+class Node(var `val`: Int) {
+    var neighbors: ArrayList<Node?> = ArrayList()
+}
+*/
+
+class Solution {
+    fun cloneGraph(node: Node?): Node? {
+        node ?: return null
+
+        val map = HashMap<Node, Node>()
+
+        fun dfs(node: Node): Node {
+            if(node in map)
+                return map[node]!!
+            
+            val clone = Node(node.`val`)
+            map[node] = clone
+
+            for(nei in node.neighbors) {
+                nei?.let {
+                    clone.neighbors.add(dfs(it))
+                }
+            }
+
+            return clone
+        }
+
+        return dfs(node)
+    }
+}
