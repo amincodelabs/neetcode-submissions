2b157476f939fe9b2e6571359097a4527cda1241
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-04-28 20:05:18 +0400

    Add: count-connected-components - submission-3

---
+++ Data Structures & Algorithms/count-connected-components/submission-3.kt
@@ -0,0 +1,42 @@
+class DSU(n: Int) {
+    val parent = IntArray(n) { it }
+    val rank = IntArray(n) { 1 }
+
+    fun find(node: Int): Int {
+        var cur = node
+        while (cur != parent[cur]) {
+            parent[cur] = parent[parent[cur]]
+            cur = parent[cur]
+        }
+        return cur
+    }
+
+    fun union(u: Int, v: Int): Boolean {
+        val pu = find(u)
+        val pv = find(v)
+        if (pu == pv) {
+            return false
+        }
+        if (rank[pv] > rank[pu]) {
+            parent[pu] = pv
+        } else {
+            parent[pv] = pu
+            rank[pu] += rank[pv]
+        }
+        return true
+    }
+}
+
+class Solution {
+    fun countComponents(n: Int, edges: Array<IntArray>): Int {
+        val dsu = DSU(n)
+        var res = n
+        for (edge in edges) {
+            val (u, v) = edge
+            if (dsu.union(u, v)) {
+                res--
+            }
+        }
+        return res
+    }
+}
