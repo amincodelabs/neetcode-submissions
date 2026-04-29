7854e3e557fc0e5e644b54acdc6590e571e07b6f
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-04-29 12:46:32 +0400

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun climbStairs(n: Int): Int {
+        val cache = IntArray(n + 1) { -1 }
+
+        fun recursive(k: Int): Int {
+        if(k > n) return 0
+        if(k == n) return 1
+        if(cache[k] != -1) return cache[k]
+
+        cache[k] = recursive(k + 1) + recursive(k + 2)
+        return cache[k]
+    }
+
+        return recursive(0)
+    }
+}
