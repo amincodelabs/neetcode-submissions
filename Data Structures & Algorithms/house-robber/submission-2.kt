ab3db6ea5fa2b67e4d9057ea2ab7c6c8b75d19cb
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-04-29 14:23:15 +0400

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun rob(nums: IntArray): Int {
+        if (nums.isEmpty()) {
+            return 0
+        }
+        if (nums.size == 1) {
+            return nums[0]
+        }
+
+        val dp = IntArray(nums.size)
+        dp[0] = nums[0]
+        dp[1] = maxOf(nums[0], nums[1])
+
+        for (i in 2 until nums.size) {
+            dp[i] = maxOf(dp[i - 1], nums[i] + dp[i - 2])
+        }
+
+        return dp[nums.size - 1]
+    }
+}
