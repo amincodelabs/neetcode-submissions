f9a6735e449458874f79628f102e3f1866a5385e
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-05-02 11:37:58 +0400

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun longestPalindrome(s: String): String {
+        var result = ""
+
+        for(i in s.indices) {
+            for(j in i until s.length) {
+                var l = i
+                var r = j
+                while(l < r && s[l] == s[r]) {
+                    l++
+                    r--
+                }
+                val k = j - i + 1
+                if(l >= r && result.length < k) {
+                    result = s.substring(i, j + 1)
+                }
+            }
+        }
+
+        return result
+    }
+}
