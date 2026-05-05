bd2129af97fd3cb999724502ceba8ce163087df9
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-05-05 11:17:34 +0400

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.kt
@@ -0,0 +1,26 @@
+// Sorting
+// Time O(nlogn)
+class Solution {
+    fun longestConsecutive(nums: IntArray): Int {
+        if (nums.isEmpty()) return 0
+        nums.sort()
+
+        var res = 0
+        var curr = nums[0]
+        var streak = 0
+        var i = 0
+        while (i < nums.size) {
+            if (curr != nums[i]) {
+                curr = nums[i]
+                streak = 0
+            }
+            while (i < nums.size && nums[i] == curr) {
+                i++
+            }
+            streak++
+            curr++
+            res = maxOf(res, streak)
+        }
+        return res
+    }
+}
