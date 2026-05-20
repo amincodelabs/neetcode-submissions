0ea3b6876255b0a96dab364b418c8b30db8484cb
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-05-20 16:25:25 +0400

    Add: invert-a-binary-tree - submission-7

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-7.py
@@ -0,0 +1,16 @@
+# Iterative DFS
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return None
+        
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+            if node.left is not None:
+                stack.append(node.left)
+            if node.right is not None:
+                stack.append(node.right)
+        return root
+        
