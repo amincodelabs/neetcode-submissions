efa93be6c838796a9fa10c27e229b3e5d54e72da
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-06-05 22:33:50 +0400

    Add: house-robber-ii - submission-6

---
+++ Data Structures & Algorithms/house-robber-ii/submission-6.cpp
@@ -0,0 +1,28 @@
+class Solution {
+   public:
+    unordered_map<int, int> cache;
+
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) return nums[0];
+        vector<int> left(nums.begin(), nums.end() - 1);
+        vector<int> right(nums.begin() + 1, nums.end());
+
+        int result1 = dfs(left, 0);
+        cache.clear();
+        int result2 = dfs(right, 0);
+
+        return max(result1, result2);
+    }
+
+   private:
+    int dfs(vector<int>& nums, int i) {
+        if (i >= nums.size()) {
+            return 0;
+        }
+        if (cache.count(i)) {
+            return cache[i];
+        }
+        cache[i] = max(dfs(nums, i + 1), nums[i] + dfs(nums, i + 2));
+        return cache[i];
+    }
+};
