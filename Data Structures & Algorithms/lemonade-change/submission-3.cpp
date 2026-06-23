ea525dfb0160b3ad497687d586436eb204f5584b
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-06-23 12:26:16 +0400

    Add: lemonade-change - submission-3

---
+++ Data Structures & Algorithms/lemonade-change/submission-3.cpp
@@ -0,0 +1,32 @@
+class Solution {
+   public:
+    bool lemonadeChange(vector<int>& bills) {
+        for (int bill : bills) {
+            int changeNeeded = bill - 5;
+            if (changeNeeded > 0) {
+                if (!canChange(changeNeeded)) return false;
+            }
+            changes[bill] = changes[bill] + 1;
+        }
+
+        return true;
+    }
+
+   private:
+    unordered_map<int, int> changes;
+
+    bool canChange(int amount) {
+        if (amount == 0) {
+            return true;
+        }
+        if (amount >= 10 && changes[10] > 0) {
+            changes[10] = changes[10] - 1;
+            return canChange(amount - 10);
+        } else if (amount >= 5 && changes[5] > 0) {
+            changes[5] = changes[5] - 1;
+            return canChange(amount - 5);
+        } else {
+            return false;
+        }
+    }
+};
