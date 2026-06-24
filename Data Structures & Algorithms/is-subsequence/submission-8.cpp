8736defdaf13d4331c2ae2f0e3ca3c9d93442db8
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-06-24 11:33:42 +0400

    Add: is-subsequence - submission-8

---
+++ Data Structures & Algorithms/is-subsequence/submission-8.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    bool isSubsequence(string s, string t) { return recursion(s, t, 0, 0); }
+
+   private:
+    bool recursion(string& s, string& t, int i, int j) {
+        if (i == s.length()) {
+            return true;
+        }
+        if (j == t.length()) {
+            return false;
+        }
+        if (s[i] == t[j]) {
+            return recursion(s, t, i + 1, j + 1);
+        }
+        return recursion(s, t, i, j + 1);
+    }
+};
