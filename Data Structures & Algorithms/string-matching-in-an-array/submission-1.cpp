0259b76b3fcba31e3a8fb40db0391c50dcba0288
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-06-26 14:43:56 +0400

    Add: string-matching-in-an-array - submission-1

---
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-1.cpp
@@ -0,0 +1,41 @@
+class Solution {
+   public:
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> result;
+
+        for (const auto& w1 : words) {
+            for (const auto& w2 : words) {
+                if (w1 == w2) {
+                    continue;
+                }
+                if (isSubstring(w1, w2)) {
+                    result.push_back(w1);
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+   private:
+    bool isSubstring(string small, string big) {
+        if (small.size() > big.size()) {
+            return false;
+        }
+
+        for (int i = 0; i <= big.size() - small.size(); i++) {
+            int j = 0;
+
+            while (j < small.size() && big[i + j] == small[j]) {
+                j++;
+            }
+
+            if (j == small.size()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
