6da3855b175bdb92198b634a5c11366d033ef816
Amin Chahardoli <107853557+amincodelabs@users.noreply.github.com>
2026-07-09 15:08:08 +0400

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+   public:
+    int calPoints(vector<string>& operations) {
+        vector<int> points;
+
+        for (const string& op : operations) {
+            if (op == "+") {
+                if (points.size() >= 2) {
+                    int a = points.back();
+                    int b = points[points.size() - 2];
+                    points.push_back(a + b);
+                }
+            } else if (op == "D") {
+                int a = points.back();
+                points.push_back(a + a);
+            } else if (op == "C") {
+                points.pop_back();
+            } else {
+                int x = stoi(op);
+                points.push_back(x);
+            }
+        }
+
+        return accumulate(points.begin(), points.end(), 0);
+    }
+};
